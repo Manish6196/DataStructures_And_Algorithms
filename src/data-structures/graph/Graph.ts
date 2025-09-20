17ce061bef264c19caec74fadcf6eaa54fb8cd2f
manish <manishrajput6196@gmail.com>
2025-09-20 14:54:55 +0530

    feat(data-structures): add graph data structure

---
+++ src/data-structures/graph/Graph.ts
@@ -0,0 +1,74 @@
+type AdjacencyList = Record<string, Set<string>>;
+
+class Graph {
+  private adjacencyList: AdjacencyList;
+
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  // has(vertex: string): vertex is keyof AdjacencyList {
+  //   return !!this.adjacencyList[vertex];
+  // }
+
+  addVertex(vertex: string) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = new Set();
+    }
+  }
+
+  removeVertex(vertex: string) {
+    if (!this.adjacencyList[vertex]) return;
+    for (const adjacentVertex of this.adjacencyList[vertex]) {
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  addEdge(vertex1: string, vertex2: string) {
+    if (!this.adjacencyList[vertex1]) this.addVertex(vertex1);
+    if (!this.adjacencyList[vertex2]) this.addVertex(vertex2);
+    this.adjacencyList[vertex1].add(vertex2);
+    this.adjacencyList[vertex2].add(vertex1);
+  }
+
+  hasEdge(vertex1: string, vertex2: string) {
+    return (
+      this.adjacencyList[vertex1].has(vertex2) &&
+      this.adjacencyList[vertex2].has(vertex1)
+    );
+  }
+
+  removeEdge(vertex1: string, vertex2: string) {
+    this.adjacencyList[vertex1].delete(vertex2);
+    this.adjacencyList[vertex2].delete(vertex1);
+  }
+
+  display() {
+    for (const vertex in this.adjacencyList) {
+      console.log(vertex, " --> ", [...this.adjacencyList[vertex]]);
+    }
+  }
+}
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+
+graph.display();
+
+console.log(graph.hasEdge("A", "B"));
+console.log(graph.hasEdge("A", "C"));
+
+// graph.removeEdge("A", "B");
+// console.log(graph.hasEdge("A", "B"));
+// graph.display();
+
+graph.removeVertex("C");
+graph.display();
+
+export default Graph;
